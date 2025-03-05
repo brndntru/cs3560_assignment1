16a57c7a36818d35ba329ba279b8b7434656f780
brndntru <112743187+brndntru@users.noreply.github.com>
2025-03-04 17:32:00 -0800

    Update validParentheses.java

--- validParentheses.java
+++ validParentheses.java
@@ -2,12 +2,20 @@ import java.util.*;
 
 public class validParentheses {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>(); // creates stack to store and track expected closed brackets
+
+        // iterates through each character to check validity of parentheses
         for (char c : s.toCharArray()) {
-            if (c == '(') stack.push(')');
-            else if (c == '{') stack.push('}');
-            else if (c == '[') stack.push(']');
-            else if (stack.isEmpty() || stack.pop() != c) return false;
+            // if open bracket, push corresponding close bracket onto stack
+            if (c == '(')
+                stack.push(')');
+            else if (c == '{')
+                stack.push('}');
+            else if (c == '[')
+                stack.push(']');
+            // if close bracket, check if matches top of stack
+            else if (stack.isEmpty() || stack.pop() != c)
+                return false; // returns false if doesn't match or stack is empty
         }
         return stack.isEmpty();
     }
