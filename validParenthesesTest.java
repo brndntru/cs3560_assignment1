4afd05b7c82aaf5d3f7913ec3477b81d21496e09
brndntru <112743187+brndntru@users.noreply.github.com>
2025-03-04 17:32:51 -0800

    Update validParenthesesTest.java

--- validParenthesesTest.java
+++ validParenthesesTest.java
@@ -4,18 +4,21 @@ import org.junit.jupiter.api.Test;
 public class validParenthesesTest {
     validParentheses vp = new validParentheses();
 
+    // valid test cases
     @Test
     public void testIsValid() {
         assertTrue(vp.isValid("()"));
         assertTrue(vp.isValid("()[]{}"));
     }
 
+    // invalid test cases
     @Test
     public void testIsInValid() {
         assertFalse(vp.isValid("(]"));
         assertFalse(vp.isValid("([)]"));
     }
 
+    // edge test cases
     @Test
     public void testEdges() {
         assertTrue(vp.isValid(""));
